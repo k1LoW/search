9d65cc77cfe9fef7f3ef1e753cbc73959066109c
k1LoW <k1lowxb@gmail.com>
2010-07-12 21:29:21 +0900

    Oops, indent modified

--- controllers/components/prg.php
+++ controllers/components/prg.php
@@ -1,88 +1,88 @@
 <?php
-  /**
-   * CakePHP Search Plugin
-   *
-   * Copyright 2009 - 2010, Cake Development Corporation
-   *						  1785 E. Sahara Avenue, Suite 490-423
-   *						  Las Vegas, Nevada 89104
-   *
-   * Licensed under The MIT License
-   * Redistributions of files must retain the above copyright notice.
-   *
-   * @copyright 2009 - 2010, Cake Development Corporation (http://cakedc.com)
-   * @link	  http://github.com/CakeDC/Search
-   * @package	  plugins.search
-   * @license	  MIT License (http://www.opensource.org/licenses/mit-license.php)
-   */
+/**
+ * CakePHP Search Plugin
+ *
+ * Copyright 2009 - 2010, Cake Development Corporation
+ *                        1785 E. Sahara Avenue, Suite 490-423
+ *                        Las Vegas, Nevada 89104
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright 2009 - 2010, Cake Development Corporation (http://cakedc.com)
+ * @link      http://github.com/CakeDC/Search
+ * @package   plugins.search
+ * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
 
-  /**
-   * Post-Redirect-Get: Transfers POST Requests to GET Requests
-   *
-   * @package		plugins.search
-   * @subpackage	plugins.search.controllers.components
-   */
+/**
+ * Post-Redirect-Get: Transfers POST Requests to GET Requests
+ *
+ * @package		plugins.search
+ * @subpackage	plugins.search.controllers.components
+ */
 class PrgComponent extends Object {
-	/**
-	 * Actions used to fetch the post data
-	 *
-	 * Maps the action that takes the post data and processes it by using this
-	 * component and maps it to another action that is accessed by a redirect which
-	 * has the post data attached as get data now
-	 *
-	 * array('search' => 'results');
-	 * array('search' => array('controller' => 'results');
-	 *
-	 * @var array actions
-	 * @access public
-	 */
+/**
+ * Actions used to fetch the post data
+ *
+ * Maps the action that takes the post data and processes it by using this
+ * component and maps it to another action that is accessed by a redirect which
+ * has the post data attached as get data now
+ *
+ * array('search' => 'results');
+ * array('search' => array('controller' => 'results');
+ *
+ * @var array actions
+ * @access public
+ */
 	public $actions = array();
 
-	/**
-	 * Enables encoding on all presetVar fields
-	 *
-	 * @var boolean
-	 * @access public
-	 */
+/**
+ * Enables encoding on all presetVar fields
+ *
+ * @var boolean
+ * @access public
+ */
 	public $encode = false;
 
-	/**
-	 * Enables change queryString on all presetVar fields
-	 *
-	 * @var boolean
-	 * @access public
-	 */
+/**
+ * Enables change queryString on all presetVar fields
+ *
+ * @var boolean
+ * @access public
+ */
 	public $queryString = false;
 
-	/**
-	 * Intialize Callback
-	 *
-	 * @param object Controller object
-	 * @access public
-	 */
+/**
+ * Intialize Callback
+ *
+ * @param object Controller object
+ * @access public
+ */
 	public function initialize(&$controller) {
 		$this->controller = $controller;
 	}
 
-	/**
-	 * Poplulates controller->data with allowed values from the named/passed get params
-	 *
-	 * Fields in $controller::$presetVars that have a type of 'lookup' the foreignKey value will be inserted
-	 *
-	 * 1) 'lookup'
-	 *	  Is used for autocomplete selectors
-	 *	  For autocomplete we have hidden field with value and autocomplete text box
-	 *	  Component fills text part on id from hidden field
-	 * 2) 'value'
-	 *	  The value as it is entered in form
-	 * 3) 'checkbox'
-	 *	  Allows to pass several values internaly encoded as string
-	 *
-	 * 1 use field, model, formField, and modelField
-	 * 2, 3 need only field parameter
-	 *
-	 * @param array
-	 * @access public
-	 */
+/**
+ * Poplulates controller->data with allowed values from the named/passed get params
+ *
+ * Fields in $controller::$presetVars that have a type of 'lookup' the foreignKey value will be inserted
+ *
+ * 1) 'lookup'
+ *    Is used for autocomplete selectors
+ *    For autocomplete we have hidden field with value and autocomplete text box
+ *    Component fills text part on id from hidden field
+ * 2) 'value'
+ *    The value as it is entered in form
+ * 3) 'checkbox'
+ *    Allows to pass several values internaly encoded as string
+ *
+ * 1 use field, model, formField, and modelField
+ * 2, 3 need only field parameter
+ *
+ * @param array
+ * @access public
+ */
 	public function presetForm($model) {
 		$data = array($model => array());
 		$args = $this->controller->passedArgs;
@@ -129,12 +129,12 @@ class PrgComponent extends Object {
 		$this->controller->parsedData = $data;
 	}
 
-	/**
-	 * Restores form params for checkboxs and other url encoded params
-	 *
-	 * @param array
-	 * @access public
-	 */
+/**
+ * Restores form params for checkboxs and other url encoded params
+ *
+ * @param array
+ * @access public
+ */
 	public function serializeParams(&$data) {
 		$queryString = $this->controller->params['url'];
 		unset($queryString['url']);
@@ -169,14 +169,14 @@ class PrgComponent extends Object {
 		return $data;
 	}
 
-	/**
-	 * Connect named arguments
-	 *
-	 * @param array $data
-	 * @param array $exclude
-	 * @return void
-	 * @access public
-	 */
+/**
+ * Connect named arguments
+ *
+ * @param array $data
+ * @param array $exclude
+ * @return void
+ * @access public
+ */
 	public function connectNamed($data = null, $exclude = array()) {
 		if (!isset($data)) {
 			$data = $this->controller->passedArgs;
@@ -196,16 +196,16 @@ class PrgComponent extends Object {
 		}
 	}
 
-	/**
-	 * Exclude
-	 *
-	 * Removes key/values from $array based on $exclude
+/**
+ * Exclude
+ *
+ * Removes key/values from $array based on $exclude
 
-	 * @param array Array of data to be filtered
-	 * @param array Array of keys to exclude from other $array
-	 * @return array
-	 * @access public
-	 */
+ * @param array Array of data to be filtered
+ * @param array Array of keys to exclude from other $array
+ * @return array
+ * @access public
+ */
 	public function exclude($array, $exclude) {
 		$data = array();
 		foreach ($array as $key => $value) {
@@ -216,24 +216,24 @@ class PrgComponent extends Object {
 		return $data;
 	}
 
-	/**
-	 * Common search method
-	 *
-	 * Handles processes common to all PRG forms
-	 *
-	 * - Handles validation of post data
-	 * - converting post data into named params
-	 * - Issuing redirect(), and connecting named parameters before redirect
-	 * - Setting named parameter form data to view
-	 *
-	 * @param string $modelName Name of the model class being used for the prg form
-	 * @param array $options Optional parameters:
-	 *	- string form Name of the form involved in the prg
-	 *	- string action The action to redirect to. Defaults to the current action
-	 *	- mixed modelMethod If not false a string that is the model method that will be used to process the data
-	 * @return void
-	 * @access public
-	 */
+/**
+ * Common search method
+ *
+ * Handles processes common to all PRG forms
+ *
+ * - Handles validation of post data
+ * - converting post data into named params
+ * - Issuing redirect(), and connecting named parameters before redirect
+ * - Setting named parameter form data to view
+ *
+ * @param string $modelName Name of the model class being used for the prg form
+ * @param array $options Optional parameters:
+ *  - string form Name of the form involved in the prg
+ *  - string action The action to redirect to. Defaults to the current action
+ *  - mixed modelMethod If not false a string that is the model method that will be used to process the data 
+ * @return void
+ * @access public
+ */
 	public function commonProcess($modelName = null, $options = array()) {
 		$defaults = array(
 						  'form' => null,
