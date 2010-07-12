de5dbfc29b83b29e72637846c289f7c05f75d2b6
k1LoW <k1lowxb@gmail.com>
2010-07-12 21:53:07 +0900

    Oops, indent modified

--- tests/cases/behaviors/searchable.test.php
+++ tests/cases/behaviors/searchable.test.php
@@ -3,16 +3,16 @@
  * CakePHP Search Plugin
  *
  * Copyright 2009 - 2010, Cake Development Corporation
- *						  1785 E. Sahara Avenue, Suite 490-423
- *						  Las Vegas, Nevada 89104
+ *                        1785 E. Sahara Avenue, Suite 490-423
+ *                        Las Vegas, Nevada 89104
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
  * @copyright 2009 - 2010, Cake Development Corporation (http://cakedc.com)
- * @link	  http://github.com/CakeDC/Search
- * @package	  plugins.search
- * @license	  MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @link      http://github.com/CakeDC/Search
+ * @package   plugins.search
+ * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
 /**
